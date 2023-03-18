5688fbdff601323d46ab2374b31da8a4505e5d12
姚孟隆 <2601226294@qq.com>
2023-03-18 12:50:46 +0800

    change default name and default introdiction for blog app

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>[A JavaScript/TypeScript Developer, Also Use Go and Rust to Build Project]</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
