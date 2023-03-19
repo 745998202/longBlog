c6a25cdac547884b376d573f4ce64ba55fab41be
姚孟隆 <2601226294@qq.com>
2023-03-19 23:47:17 +0800

    add image

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getAllMdxData } from '../lib/posts'
 import Link from 'next/link'
+import Image from 'next/image'
 import Date from '../components/date'
+const name = 'Menglong Yao'
 
 export default function Home({ allPostsData }) {
   return (
@@ -16,6 +18,13 @@ export default function Home({ allPostsData }) {
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
+        <Image
+              priority
+              src="/images/photo1.jpg"
+              height={300}
+              width={300}
+              alt={name}
+            />
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
